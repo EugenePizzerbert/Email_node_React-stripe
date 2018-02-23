c5213d00760dc4fe02a02b97cbd5b775bada2045
Akbar zubairi <akbarj@gmail.com>
2018-02-22 20:48:24 -0500

    made changesto cofig for production

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+
+if (process.env.NODE_ENV === 'production'){
+  module.exports = require('./prod')
+}
+else {
+  module.exports = require('./dev')
+}
+
+//
+// clientID 146022585957-mtts8ktqkoo8sgt28d4ajt04oeb80e0p.apps.googleusercontent.com
+// clientSecret qRHPlZ8KYi2Pn_Z8i2q1VnvN
