fa7d3923a34c809904e1bbdf1aeaa14ababf9e81
Akbar zubairi <akbarj@gmail.com>
2018-02-28 00:25:28 -0500

    frond

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,6 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripeKey: process.env.STRIPE_KEY,
-  sendGridKey: process.env.SENDGRID_API_KEY
+  sendGridKey:
+    "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"
 };
