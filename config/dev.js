b1de263abc98a9272876521a6fb2bfc57e2244db
Akbar zubairi <akbarj@gmail.com>
2018-03-06 00:19:39 -0500

    First-dev Commit

--- config/dev.js
+++ config/dev.js
@@ -1,14 +1,4 @@
-module.exports = {
-  googleClientID:
-    "146022585957-mtts8ktqkoo8sgt28d4ajt04oeb80e0p.apps.googleusercontent.com",
-  googleClientSecret: "qRHPlZ8KYi2Pn_Z8i2q1VnvN",
-  mongoURI: "mongodb://akbarj:success20@ds243728.mlab.com:43728/emaily-dev",
-  cookieKey: "dskljlkjfsdkljfksjdfjslkjfkdjskjlsdjfdksjfkjsl",
-  stripeKey: "sk_test_PMJ39mEnNdHcljq7SPK6K8hz",
-  sendGridKey:
-    "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"
-};
-
+this is a test
 //this is old stuff
 
 //mighty-savannah-50823
