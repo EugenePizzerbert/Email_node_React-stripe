c52dc386b5ae99681652f74472bd22ed8792d579
Akbar zubairi <akbarj@gmail.com>
2018-03-06 01:53:21 -0500

    first readme

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,2 @@
-module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENTID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  stripeKey: process.env.STRIPE_KEY,
-  sendGridKey: process.env.SENDGRID_API_KEY
-};
+//Pleae Use your own key here..
+//follow prod.js instructions
