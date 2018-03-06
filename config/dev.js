f2a2b1693d6f4de7becc0dd596135a49127f7c3b
Akbar Zubairi <akbarj@gmail.com>
2018-03-06 00:24:03 -0500

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,8 @@
-this is a test
-//this is old stuff
-
-//mighty-savannah-50823
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENTID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  stripeKey: process.env.STRIPE_KEY,
+  sendGridKey: process.env.SENDGRID_API_KEY
+};
